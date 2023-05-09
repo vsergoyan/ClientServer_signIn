837250d9d5a2b2f3efb7e96357411f214395d832
vsergoyan <vardeni.sergoyan1@gmail.com>
2023-05-09 21:46:51 +0400

    Added functionalities for encrypt

--- src/MainClient.cpp
+++ src/MainClient.cpp
@@ -1,10 +1,160 @@
-/*
+ /*
 	UDP Client routine
 */
 
 #include "UDPSocket.h"
 //#include "source.h"
 #include <stdio.h>
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+//using namespace std;
+
+std::string generateKey(std::string str, std::string key)
+{
+	int x = str.size();
+	for (int i = 0; ; i++)
+	{
+		if (x == i)
+			i = 0;
+		if (key.size() == str.size())
+			break;
+		key.push_back(key[i]);
+	}
+	return key;
+}
+
+std::string Decoder(std::string str, std::string key)
+{
+	std::string result;
+	for (int i = 0; i < str.size(); i++)
+	{
+		char x = (str[i] + key[i]) % 26;
+		x += 'A';
+		result.push_back(x);
+	}
+	return result;
+}
+
+std::string Ncoder(std::string decod_string, std::string key)
+{
+	std::string orig_string;
+	for (int i = 0; i < decod_string.size(); i++)
+	{
+		char x = (decod_string[i] - key[i] + 26) % 26;
+		x += 'A';
+		orig_string.push_back(x);
+	}
+	return orig_string;
+}
+
+
+struct Node {
+	std::string data;
+	int freq;
+	Node* left;
+	Node* right;
+
+	Node(std::string data, int freq) {
+		this->data = data;
+		this->freq = freq;
+		this->left = nullptr;
+		this->right = nullptr;
+	}
+
+	~Node() {
+		delete left;
+		delete right;
+	}
+};
+
+struct CompareNodes {
+	bool operator()(Node* n1, Node* n2) {
+		return n1->freq > n2->freq;
+	}
+};
+
+void encode(Node* root, std::string code, std::unordered_map<std::string, std::string>& codes) {
+	if (root == nullptr) {
+		return;
+	}
+
+	if (root->left == nullptr && root->right == nullptr) {
+		codes[root->data] = code;
+	}
+
+	encode(root->left, code + "0", codes);
+	encode(root->right, code + "1", codes);
+}
+
+std::unordered_map<std::string, std::string> huffman(std::string str) {
+	// count frequency of each character
+	std::unordered_map<std::string, int> freq;
+	for (char c : str) {
+		freq[std::string(1, c)]++;
+	}
+
+	// create a priority queue of nodes
+	std::priority_queue<Node*, std::vector<Node*>, CompareNodes> pq;
+	for (auto p : freq) {
+		pq.push(new Node(p.first, p.second));
+	}
+
+	// build the Huffman tree
+	while (pq.size() > 1) {
+		Node* n1 = pq.top();
+		pq.pop();
+		Node* n2 = pq.top();
+		pq.pop();
+
+		Node* parent = new Node("", n1->freq + n2->freq);
+		parent->left = n1;
+		parent->right = n2;
+
+		pq.push(parent);
+	}
+
+	// encode the characters
+	std::unordered_map<std::string, std::string> codes;
+	Node* root = pq.top();
+	encode(root, "", codes);
+	delete root;
+
+	return codes;
+}
+
+bool includes(std::string temp, std::unordered_map<std::string, std::string> codes) {
+	for (auto p : codes) {
+		if (temp == p.second) {
+			return true;
+		}
+	}
+	return false;
+}
+
+std::string Dcode(std::unordered_map<std::string, std::string> codes, std::string str) {
+	std::string temp = "";
+	std::string result1 = "";
+	bool inc = false;
+	int i = 0;
+	while (str[i]) {
+		while (!inc) {
+			temp.push_back(str[i]);
+			i++;
+			inc = includes(temp, codes);
+		}
+		for (auto p : codes) {
+			if (temp == p.second) {
+				result1.append(p.first);
+				break;
+			}
+		}
+		inc = false;
+		temp = "";
+	}
+	return result1;
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -17,9 +167,16 @@ int main(int argc, char* argv[])
 
 	char buf[BUFLEN];
 	char message[BUFLEN];
+	char encrypted[BUFLEN];
 	char username[BUFLEN];
 	char answ[BUFLEN];
 	char password[BUFLEN];
+
+	std::string keyword = "LIMON";
+	std::string key;
+	std::string decod_string, ncode_string;
+	std::string decod_stringg = "PZQJNYQEHUPKMDVEIXCSLZYSATI";
+
 
 	unsigned short srv_port = 0;
 	char srv_ip_addr[40];
@@ -52,49 +209,6 @@ int main(int argc, char* argv[])
 		printf("3: Server - addr=%s , port=%d\n", argv[1], atoi(argv[2]));
 	}
 
-
-
-	///////////////////////////////////////////////
-
-
-	/*memset((char*)&si_other1, 0, sizeof(si_other1));
-	si_other1.sin_family = AF_INET;
-
-	if (1 == argc)
-	{
-		si_other1.sin_port = htons(PORT);
-		si_other1.sin_addr.S_un.S_addr = inet_addr(SERVER);
-		printf("1: Server - addr=%s , port=%d\n", SERVER, PORT);
-	}
-	else if (2 == argc)
-	{
-		si_other1.sin_port = htons(atoi(argv[1]));
-		si_other1.sin_addr.S_un.S_addr = inet_addr(SERVER);
-		printf("2: argv[0]: Server - addr=%s , port=%d\n", SERVER, atoi(argv[1]));
-	}
-	else
-	{
-		si_other1.sin_port = htons(atoi(argv[2]));
-		si_other1.sin_addr.S_un.S_addr = inet_addr(argv[1]);
-		printf("3: Server - addr=%s , port=%d\n", argv[1], atoi(argv[2]));
-	}*/
-
-	//start communication  lifecircle
-
-	//if (signInClient(client_sock, si_other, slen))
-	//{
-	//	printf("OK!!!\n");
-	//	while (1)
-	//	{
-	//		memset(buf, '\0', BUFLEN);
-	//		memset(message, '\0', BUFLEN);
-	//		printf("Enter message: ");
-	//		gets_s(message, BUFLEN);
-	//		client_sock.SendDatagram(message, (int)strlen(message), (struct sockaddr*)&si_other1, slen1);
-	//		client_sock.RecvDatagram(buf, BUFLEN, (struct sockaddr*)&si_other1, &slen1);
-	//		printf("Answer: %s\n", buf);
-	//	}
-	//}
 	while (1)
 	{
 		memset(answ, '\0', BUFLEN);
@@ -119,11 +233,37 @@ int main(int argc, char* argv[])
 				printf("OK!!!\n");
 				while (1)
 				{
+					//popoxutyunner
 					printf("Enter message: ");
 					gets_s(message, BUFLEN);
+					std::string strForDcode(message);
+					key = generateKey(strForDcode, keyword);
+					decod_string = Decoder(strForDcode, key);
+
+					std::string result = "";
+					std::unordered_map<std::string, std::string> codes1 = huffman(decod_string);
+
+					for (char c : decod_string) {
+						std::string s(1, c);
+						for (auto p : codes1) {
+							if (s == p.first) {
+								result.append(p.second);
+							}
+						}
+					}
+					strcpy_s(message, result.c_str());
 					client_sock.SendDatagram(message, (int)strlen(message), (struct sockaddr*)&si_other, slen);
+					memset(message, '\0', BUFLEN);
+
 					client_sock.RecvDatagram(buf, BUFLEN, (struct sockaddr*)&si_other, &slen);
+					std::unordered_map<std::string, std::string> codes2 = huffman(decod_stringg);
+					std::string decod_string(buf);
+					std::string strForNcode = Dcode(codes2, decod_string);
+					key = generateKey(strForNcode, keyword);
+					ncode_string = Ncoder(strForNcode, key);
+					strcpy_s(buf, ncode_string.c_str());
 					printf("Answer: %s\n", buf);
+					memset(buf, '\0', BUFLEN);
 				}
 			}
 			else
--- src/MainServer.cpp
+++ src/MainServer.cpp
@@ -4,6 +4,10 @@
 
 #include <stdio.h>
 #include "UDPServer.h"
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+//using namespace std;
 //#include "source.h"
 
 #ifndef _WIN32
@@ -18,12 +22,158 @@ using SOCKET = int
 #define BUFLEN 	1024		    //Max length of buffer
 #define PORT    8888			//The port on which to listen for incoming data
 
+std::string generateKey(std::string str, std::string key)
+{
+	int x = str.size();
+	for (int i = 0; ; i++)
+	{
+		if (x == i)
+			i = 0;
+		if (key.size() == str.size())
+			break;
+		key.push_back(key[i]);
+	}
+	return key;
+}
+
+std::string Decoder(std::string str, std::string key)
+{
+	std::string result;
+	for (int i = 0; i < str.size(); i++)
+	{
+		char x = (str[i] + key[i]) % 26;
+		x += 'A';
+		result.push_back(x);
+	}
+	return result;
+}
+
+std::string Ncoder(std::string decod_string, std::string key)
+{
+	std::string orig_string;
+	for (int i = 0; i < decod_string.size(); i++)
+	{
+		char x = (decod_string[i] - key[i] + 26) % 26;
+		x += 'A';
+		orig_string.push_back(x);
+	}
+	return orig_string;
+}
+
+
+struct Node {
+	std::string data;
+	int freq;
+	Node* left;
+	Node* right;
+
+	Node(std::string data, int freq) {
+		this->data = data;
+		this->freq = freq;
+		this->left = nullptr;
+		this->right = nullptr;
+	}
+
+	~Node() {
+		delete left;
+		delete right;
+	}
+};
+
+struct CompareNodes {
+	bool operator()(Node* n1, Node* n2) {
+		return n1->freq > n2->freq;
+	}
+};
+
+void encode(Node* root, std::string code, std::unordered_map<std::string, std::string>& codes) {
+	if (root == nullptr) {
+		return;
+	}
+
+	if (root->left == nullptr && root->right == nullptr) {
+		codes[root->data] = code;
+	}
+
+	encode(root->left, code + "0", codes);
+	encode(root->right, code + "1", codes);
+}
+
+std::unordered_map<std::string, std::string> huffman(std::string str) {
+	// count frequency of each character
+	std::unordered_map<std::string, int> freq;
+	for (char c : str) {
+		freq[std::string(1, c)]++;
+	}
+
+	// create a priority queue of nodes
+	std::priority_queue<Node*, std::vector<Node*>, CompareNodes> pq;
+	for (auto p : freq) {
+		pq.push(new Node(p.first, p.second));
+	}
+
+	// build the Huffman tree
+	while (pq.size() > 1) {
+		Node* n1 = pq.top();
+		pq.pop();
+		Node* n2 = pq.top();
+		pq.pop();
+
+		Node* parent = new Node("", n1->freq + n2->freq);
+		parent->left = n1;
+		parent->right = n2;
+
+		pq.push(parent);
+	}
+
+	// encode the characters
+	std::unordered_map<std::string, std::string> codes;
+	Node* root = pq.top();
+	encode(root, "", codes);
+	delete root;
+
+	return codes;
+}
+
+bool includes(std::string temp, std::unordered_map<std::string, std::string> codes) {
+	for (auto p : codes) {
+		if (temp == p.second) {
+			return true;
+		}
+	}
+	return false;
+}
+
+std::string Dcode(std::unordered_map<std::string, std::string> codes, std::string str) {
+	std::string temp = "";
+	std::string result1 = "";
+	bool inc = false;
+	int i = 0;
+	while (str[i]) {
+		while (!inc) {
+			temp.push_back(str[i]);
+			i++;
+			inc = includes(temp, codes);
+		}
+		for (auto p : codes) {
+			if (temp == p.second) {
+				result1.append(p.first);
+				break;
+			}
+		}
+		inc = false;
+		temp = "";
+	}
+	return result1;
+}
+
+
 int main(int argc, char* argv[])
 {
 	struct sockaddr_in si_other;
 	//struct sockaddr_in si_other1;
 	unsigned short srvport;
-	int slen, slen1;
+	int slen;
 	char buf[BUFLEN];
 	char username[BUFLEN];
 	char password[BUFLEN];
@@ -34,31 +184,16 @@ int main(int argc, char* argv[])
 	char ps[2][BUFLEN] = { {"12345678"}, {"12345678"} };
 	int i, k = 0;
 
+	std::string keyword = "LIMON";
+	std::string key;
+	std::string ncode_string;
+	std::string decod_stringg = "PZQJNYQEHUPKMDVEIXCSLZYSATI";
+
 	srvport = (1 == argc) ? PORT : atoi(argv[1]);
 
 	UDPServer server(srvport);
 	slen = sizeof(si_other);
-	//slen1 = sizeof(si_other1);
 
-	//keep listening for data
-
-	//memset((char*)&si_other, 0, sizeof(si_other));
-
-	//if (signInServer(server, si_other, slen))
-	//{
-	//	while (1)
-	//	{
-	//		memset(msg, '\0', BUFLEN);
-	//		memset(buf, '\0', BUFLEN);
-	//		//memset((char*)&si_other, 0, sizeof(si_other));
-	//		printf("Waiting for data...   ");
-	//		server.RecvDatagram(buf, BUFLEN, (struct sockaddr*)&si_other1, &slen1);
-	//		printf("%s ", buf);
-	//		printf("\nAnswer : ");
-	//		gets_s(msg, BUFLEN);
-	//		server.SendDatagram(msg, (int)strlen(msg), (struct sockaddr*)&si_other1, slen1);
-	//	}
-	//}
 	while (1)
 	{
 		memset(username, '\0', BUFLEN);
@@ -87,10 +222,35 @@ int main(int argc, char* argv[])
 					{
 						printf("Waiting for data...   ");
 						server.RecvDatagram(buf, BUFLEN, (struct sockaddr*)&si_other, &slen);
+						std::unordered_map<std::string, std::string> codes1 = huffman(decod_stringg);
+						std::string decod_string(buf);
+						std::string strForNcode = Dcode(codes1, decod_string);
+
+						key = generateKey(strForNcode, keyword);
+						ncode_string = Ncoder(strForNcode, key);
+						strcpy_s(buf, ncode_string.c_str());
 						printf("%s ", buf);
+						memset(buf, '\0', BUFLEN);
+
 						printf("\nAnswer : ");
 						gets_s(msg, BUFLEN);
+						std::string strForDcode(msg);
+						key = generateKey(strForDcode, keyword);
+						decod_string = Decoder(strForDcode, key);
+						std::string result = "";
+						std::unordered_map<std::string, std::string> codes2 = huffman(decod_string);
+
+						for (char c : decod_string) {
+							std::string s(1, c);
+							for (auto p : codes2) {
+								if (s == p.first) {
+									result.append(p.second);
+								}
+							}
+						}
+						strcpy_s(msg, result.c_str());
 						server.SendDatagram(msg, (int)strlen(msg), (struct sockaddr*)&si_other, slen);
+						memset(msg, '\0', BUFLEN);
 					}
 				}
 				else
